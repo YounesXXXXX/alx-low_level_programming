792fd901524b805c05ed3b62665c86b7a661cc6c
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-05 16:58:56 -0700

    New Files Added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description:
+ * This is the main function that demonstrates the usage of the puts function.
+ * It prints a string to the standard output followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
