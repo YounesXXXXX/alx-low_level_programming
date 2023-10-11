162b274a50cc91542edf1de1e0de30547db42b42
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-10 18:15:58 -0700

    Add 3-islower.c file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <ctype.h>
 /**
  * _islower - Checks if a character is lowercase.
  * @c: The character to be checked.
@@ -8,5 +8,5 @@
  */
 int _islower(int c)
 {
-	return islower(c);
+	return (islower(c));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <ctype.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
