2ff93cefbe2be324a69a3fa9bdaff18626cb20ec
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-10 18:54:20 -0700

    Add 5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,15 @@ int print_sign(int n)
 	{
 		return (1);
 		write(1, "+", 1);
-	} else if (n == 0)
-		{
-			return (0);
-			write(1, "0", 1);
-		}
-		else (n < 0)
-		{
-			return (-1);
-			write(1, "-", 1);
-		}
+	}
+	else if (n == 0)
+	{
+		return (0);
+		write(1, "0", 1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		write(1, "-", 1);
+	}
 }
