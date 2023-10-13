af2d9dc5baa00343f21a32053b19238dfe03836f
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-12 17:20:19 -0700

    Add 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9 and print a newline
+ * Author : YounesXXXXX
+ */
+
+void print_numbers(void)
+{
+	for ( i = 0; i >= 0 && i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
