70b8f048dd0b38a380f6cacc64b6bd2f31983240
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-12 18:26:17 -0700

    Add 5-more_numbers.c file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,15 @@ void more_numbers(void)
 	int i;
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
