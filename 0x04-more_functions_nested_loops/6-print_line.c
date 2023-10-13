6808d516f487bc56a12920fb5fbb2f7eb76342ad
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-12 18:51:09 -0700

    Add 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** print_line - draws a straight line
+ * Author : Younes XXXXX
+ */
+
+void print_line(int n)
+{
+	int n;
+	int j;
+	j = n;
+
+	if (n > 0)
+	{
+		for (n = 0; n < j - 1; n++)
+		{
+			_putchar(_);
+		}
+	_putchar('\n');
+}
