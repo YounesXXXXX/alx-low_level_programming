ad1d6d221d494651ce6c80eba50f908da277e2f3
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-16 14:01:08 -0700

    Add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to the string.
+ * Author : YounesXXXXX
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
