96b340ca8721ce2f5bedc213bf3f671aef48b5a5
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-16 14:02:53 -0700

    Add 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,6 +4,7 @@
  * _strlen - Calculates the length of a string.
  * @s: Pointer to the string.
  * Author : YounesXXXXX
+ * Return: The length of the string (number of characters excluding the null terminator).
  */
 
 int _strlen(char *s)
