0d95d30d743fb7001ffd93bf63e46983f7bd2662
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-16 14:35:03 -0700

    Add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * Return : Always 0
+ * @str : Value of string provided.
+ * Author : YounesXXXXX
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
