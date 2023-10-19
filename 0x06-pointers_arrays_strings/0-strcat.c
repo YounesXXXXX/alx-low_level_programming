ca53fd18225a21a93832daa101d5dff24de10f46
YounesXXXXX <144718552+YounesXXXXX@users.noreply.github.com>
2023-10-19 11:34:31 -0700

    Add 1-strncat.c 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the destination string (`dest`).
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int n = 0;
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * _strncat - Concatenates two strings with a limit on the number of characters.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of characters to concatenate from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
